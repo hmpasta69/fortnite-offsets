8418cf50f04d5781a70e508a87f463529cf76d26
H&M - Pasta <hmizo24310@gmail.com>
2025-07-09 22:45:56 +0200

    Update Latest Offsets.h

--- Latest Offsets.h
+++ Latest Offsets.h
@@ -1,11 +1,15 @@
 #pragma once
-#define UWORLD 0x185565C8   and   0x17E76B70 (Both right)
+#define UWORLD 0x186C09C8   and   0x17FE0B70 (Both right) [UPDATED!]
+#decine GNAMES 0x189C6A40 [UPDATED!]
+#define StaticFindObject 0x21EEB40 [UPDATED!]
+#define ProcessEvent 0x240045C (Index: 0x2f) [UPDATED!]
 #define PERSISTENT_LEVEL 0x40
 #define GAME_INSTANCE 0x248                       
 #define GAME_STATE 0x1D0                          
 #define LOCAL_PLAYERS 0x38
 #define PLAYER_CONTROLLER 0x30
 #define PLAYERCAMERAMANAGER 0x360
+#define ACTORS 0x210 [UPDATED!]
 #define LOCAL_PAWN 0x350
 #define ACKNOWLEDGED_PAWN 0x350                   
 #define PLAYER_FOV 0x3AC                           
@@ -14,11 +18,12 @@
 #define IS_LOCAL_PLAYER_CONTROLLER 0x6C4
 #define LOCATION_UNDER_RETICLE 0x2F78
 #define TARGETED_FORTPAWN 0x18D0
-#define WEAPON_CURRENT 0xAF0
 #define PLAYER_STATE 0x2C8                          
 #define CACHED_BOUNDS 0x808                        
 #define ROOT_COMPONENT 0x1B0
 #define RELATIVE_LOCATION 0x140
+#define ProjectileSpeed 0x1d68, 0x1fd4, 0x2594 (all are correct) [UPDATED!]
+#define ProjectileGravity 0x1fd8 [UPDATED!]
 #define VELOCITY 0x180
 #define CURRENT_WEAPON 0xAF0                       
 #define ISDYING 0x728
@@ -36,7 +41,7 @@
 #define KILL_SCORE 0x12A8
 #define PLAYER_ID 0x2AC
 #define PLAYER_ARRAY 0x2C0
-#define WEAPON_DATA 0x6E8                           
+#define WEAPON_DATA 0x588 [UPDATED!]                           
 #define DISPLAY_NAME 0x40
 #define WEAPON_NAME 0x40
 #define AMMO_COUNT 0x127C
@@ -49,7 +54,7 @@
 #define LAST_SUBMIT_TIME_ON_SCREEN 0x32C
 #define CAMERA_LOCATION 0x178
 #define CAMERA_ROTATION 0x188
-#define CAMERA_SECONDS 0x198
+#define WorldTimeSeconds 0x198
 #define DEFAULTFOV 0x2BC
 #define BASEFOV 0x370
 #define LEVELDISPLAY 0x128C
